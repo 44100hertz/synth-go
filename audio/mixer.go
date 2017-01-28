4b2c9acb2408c7875a3bc88aa7626d97e02cdd3f
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2017-01-28 09:59:05 -0600

    style changes, pitch fix

--- audio/mixer.go
+++ audio/mixer.go
@@ -3,14 +3,16 @@ package audio
 import "math"
 
 // The number of channel pairs, or mixer chans
-const NUM_CHANS int = 4
+const numChans int = 4
 
 type Mixer struct {
 	srate    int
 	seq      func(chan int)
 	wave     func(int) int16
-	channels *[NUM_CHANS*2]Channel
-	chans    *[NUM_CHANS](chan int16)
+	channels *[numChans * 2]Channel
+	chans    *[numChans](chan int16)
+	count    uint64
+	nextTick uint64
 }
 
 // A single playback channel. Every even channel is "L", odd "R"
@@ -21,36 +23,41 @@ type Channel struct {
 
 // Start up the parts of a context that a user needn't touch.
 func Init(seq func(chan int), wave func(int) int16, output chan int16) {
-	m := Mixer{48000, seq, wave,
-		new([NUM_CHANS*2]Channel),
-		new([NUM_CHANS]chan int16),
-	}
+	m := new(Mixer)
+	m.srate = 48000
+	m.seq = seq
+	m.channels = new([numChans * 2]Channel)
+	m.chans = new([numChans]chan int16)
 
 	for i := range m.chans {
 		m.chans[i] = make(chan int16)
 		go m.startPair(i)
 	}
 
-	var mix int16
 	for {
-		mix = 0
+		var mix int32 = 0
 		for i := range m.chans {
-			mix += <-m.chans[i]
+			mix += int32(<-m.chans[i])
 		}
-		output <- mix
+		output <- int16(mix >> 2)
+		m.count++
 	}
-}
-
-// Return increment amount to produce a specific pitch
-func (m Mixer) getPointPeriod(len uint32, note int) uint32 {
-	return uint32(float64(len) * math.Pow(float64(note), -2))
 }
 
 // Update a pair of channels
 func (m Mixer) startPair(i int) {
 	l := m.channels[i*2]
+	l.len = 0x8000
+	l.period = m.getPointPeriod(l.len, 60)
 	for {
-		l.phase = (l.phase + l.period) % (l.len + 1)
+		l.phase = (l.phase + l.period) % (l.len)
 		m.chans[i] <- m.wave(l.phase)
 	}
+}
+
+// Calculate amount to add to phase to produce a given pitch
+func (m Mixer) getPointPeriod(len int, note int) int {
+	rate := float64(len) / float64(m.srate)
+	pitch := math.Pow(2, float64(note-60)) * 440
+	return int(rate * pitch)
 }
