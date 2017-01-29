07a17088fa4b8212fb32fa024a1f8ee1caf2364d
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2017-01-29 02:06:13 -0600

    driving a different mixer

--- audio-test.go
+++ audio-test.go
@@ -6,16 +6,17 @@ import (
 
 func main() {
 	// Fill the sequence channel with a sequence C4->C5
-	getSeq := func(seq chan int) {
-		for i := 60; i < 72; i++ {
+	seq := make(chan int)
+	go func() {
+		for i := 30; i < 90; i++ {
 			seq <- i
 		}
 		close(seq)
-	}
+	}()
 
 	// Initialize mixer data with wave and sequence
 	output := make(chan int16)
-	go audio.Init(getSeq, audio.Sine, output)
+	go audio.Init(seq, audio.Waves, output)
 
 	/* Start the mixer running */
 	for i := 0; i < 480000; i++ {
