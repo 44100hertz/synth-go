eea8f27a66509d7940cd3f4d5d02d464d73f091e
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2017-01-31 14:38:35 -0600

    exporting mixer values

--- audio/mixer.go
+++ audio/mixer.go
@@ -1,6 +1,10 @@
 /* Does all of the work of the audio chip, which is really just a
  * mixer with added features.  Everything else is in this package is not
  * considered a part of the chip, but helps to use it.
+ *
+ * For most uint64's here, top 32 bits are used, bottom 32 are like a counter.
+ * This should probably be altered to use all bits of uint32 and just cope
+ * with the imprecision and limits.
  */
 
 package audio
@@ -11,32 +15,40 @@ import "math"
 const NumChans int = 4
 
 type Mixer struct {
-	// Passed-in, user-controlled params
-	wave func(int, uint32) int16
+	// Passed-in values
+	srate uint64
+	wave  func(int, uint32) int16
 
-	// Calculated values
-	srate                               uint64
-	channel                             *[NumChans * 2]channel
-	chans                               *[NumChans](chan int16)
-	tickCount, bpm, tickRate, tickSpeed uint64
-	count, nextTick                     uint64
+	// External values
+	Channel   *[NumChans * 2]Channel
+	Bpm       uint64
+	TickRate  uint64
+	TickSpeed uint64
+
+	// Internal values
+	chans     *[NumChans](chan int16)
+	count     uint64
+	tickCount uint64
+	nextTick  uint64
 }
 
 // Internal channel data
-type channel struct {
-	wave, note         int
-	len, phase, period uint64 // Top 32 bits are used, bottom 32 are like a counter
+type Channel struct {
+	Wave       int
+	Note       int
+	Period     uint64
+	Len, Phase uint64
 }
 
 // Create and run a mixer
 func NewMixer(wave func(int, uint32) int16) Mixer {
 	return Mixer{
 		wave:      wave,
-		channel:   new([NumChans * 2]channel),
+		Channel:   new([NumChans * 2]Channel),
 		chans:     new([NumChans]chan int16),
-		bpm:       120,
-		tickRate:  1,
-		tickSpeed: 1,
+		Bpm:       120,
+		TickRate:  1,
+		TickSpeed: 1,
 	}
 }
 
@@ -51,7 +63,7 @@ func (m *Mixer) Start(output chan int16, srate uint64) {
 		if m.count == m.nextTick {
 			m.tick()
 		}
-		if m.tickCount == m.tickSpeed {
+		if m.tickCount == m.TickSpeed {
 			// Load sequence data here
 		}
 		var mix int32 = 0
@@ -65,29 +77,31 @@ func (m *Mixer) Start(output chan int16, srate uint64) {
 }
 
 func (m *Mixer) tick() {
-	for i := range m.channel {
-		c := &m.channel[i]
-		c.period = m.getPointPeriod(c.len, c.note)
+	for i := range m.Channel {
+		c := &m.Channel[i]
+		c.Period = m.getPointPeriod(c.Len, c.Note)
 	}
-	m.nextTick = 60*m.srate/m.bpm/m.tickRate + m.count
+	m.nextTick = 60*m.srate/m.Bpm/m.TickRate + m.count
 	m.tickCount++
 }
 
-// Update a pair of channels
+// Update a pair of Channels
 func (m *Mixer) startPair(i int) {
-	l := &m.channel[i*2]
+	l := &m.Channel[i*2]
 	// basic test code
-	l.len = 0xffff << 32
-	l.note = 60
+	l.Len = 0xffff << 32
+	l.Note = 60
 	for {
-		l.phase = (l.phase + l.period) % l.len
-		m.chans[i] <- m.wave(l.wave, uint32(l.phase>>32))
+		l.Phase = (l.Phase + l.Period) % l.Len
+		m.chans[i] <- m.wave(l.Wave, uint32(l.Phase>>32))
 	}
 }
 
 // Calculate amount to add to phase to produce a given pitch
 func (m *Mixer) getPointPeriod(len uint64, note int) uint64 {
-	rate := float64(len>>11) / float64(m.srate) // point period for 1hz wave
+	// Find point period for 1hz wave at given length
+	rate := len / m.srate
+	// Find desired pitch in hertz
 	pitch := math.Pow(2, float64(note-60)/12.0) * 440
-	return uint64(rate*pitch) << 11
+	return uint64(float64(rate) * pitch)
 }
