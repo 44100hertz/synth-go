cef352088e9c77248f8d6cdbcdbb1f8e96010df8
Sam Pagenkopf <ssaammp@gmail.com>
2017-02-01 11:24:53 -0600

    testing filter

--- audio-test.go
+++ audio-test.go
@@ -8,9 +8,13 @@ func main() {
 	}
 	m := audio.NewMixer(audio.Waves, seq)
 	m.Ch[0].TuneSlide = 0xff0
-	m.ResetLevels(0.5)
+	m.ResetLevels(1.0)
 	m.Ch[2].Vol = 0
 	m.Ch[0].Delay = 0x1000
-	m.Ch[0].DelayLevel = 0xC000
+	m.Ch[0].Wave = 2
+	m.Ch[0].Note = 60
+	m.Ch[0].Delay = 0x60
+	m.Ch[0].DelayLevel = 0x10000
+	m.Ch[0].DelayFilter = 0x2
 	audio.Start(&m)
 }
