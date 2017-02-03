71ea96cd88d3e954ce65390949d9dc53f7a0c7ab
Sam Pagenkopf <ssaammp@gmail.com>
2017-02-03 00:30:38 -0600

    19-tone delay scale reverb thing

--- audio-test.go
+++ audio-test.go
@@ -3,26 +3,24 @@ package main
 import "./audio"
 
 func main() {
-	counter := int32(0)
+	counter := int32(120)
 	seq := func(m *audio.Mixer) {
 		m.OnPair(0, func(c *audio.Channel) {
-			if counter%12 < 11 {
+			if counter%6 < 5 {
 				c.Vol = 0
 			} else {
-				c.DelayNote = counter / 3
-				c.Vol = 0x10000
+				c.Note = (counter << 16) * 16 / 19 / 3
+				c.DelayTicks = 1
+				c.Vol = 0x8000
 			}
 		})
 		counter++
 	}
 	m := audio.NewMixer(audio.Waves, seq)
 	m.OnPair(0, func(c *audio.Channel) {
-		c.Vol = 0x8000        // Full volume (single channel)
-		c.Note = 1            // C#-0 as base note
-		c.DelayLevel = 0xFFF0 // A bit of delay attenuation
-		c.Filter = 0x5        // Use a delay averaged by 3 samples
+		c.DelayVol = 0x8000 // A bit of delay attenuation
+		c.Filter = 0x5      // Use a delay averaged by 3 samples
 		c.Wave = 3
-		c.DelayNote = 48 << 16
 	})
 
 	audio.Start(&m)
