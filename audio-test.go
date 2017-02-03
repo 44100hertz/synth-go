a574f4cd66c2eb6deb82e7a0115712eacd08ef22
Sam Pagenkopf <ssaammp@gmail.com>
2017-02-02 21:00:44 -0600

    guitar sounds

--- audio-test.go
+++ audio-test.go
@@ -3,29 +3,31 @@ package main
 import "./audio"
 
 func main() {
+	counter := int32(0)
 	seq := func(m *audio.Mixer) {
-		m.Ch[1].DelayNote++
-		// m.OnPair(0, func(c *audio.Channel) {
-		// 	c.Vol = 0
-		// })
+		m.OnPair(0, func(c *audio.Channel) {
+			if counter%12 < 11 {
+				c.Vol = 0
+			} else {
+				c.DelayNote += 1 << 16
+				c.Vol = 0x10000
+			}
+		})
+		counter++
 	}
 	m := audio.NewMixer(audio.Waves, seq)
 	m.OnPair(0, func(c *audio.Channel) {
-		c.Vol = 0x1000 // Full volume (single channel)
+		c.Vol = 0x8000 // Full volume (single channel)
 		// c.Note = 1            // C#-0 as base note
 		// c.DelayLevel = 0xFFF0 // A bit of delay attenuation
 		// c.Filter = 0x3        // Use a delay averaged by 3 samples
+		c.Wave = 2
+		c.Note = 1
+		c.DelayLevel = 0xFFF0
+		c.DelayNote = 48 << 16
+		c.Filter = 3
 	})
-	m.Ch[0].Wave = 0
-	m.Ch[0].PairMode = audio.PAIR_PM
-	m.Ch[0].VolSlide = 0x200
 
-	m.Ch[1].Wave = 1
-	m.Ch[1].DelayLevel = 0x10000
-	m.Ch[1].DelayNote = 30
-	m.Ch[1].Filter = 3
-	m.Ch[1].VolSlide = -0x1
-
-	//	audio.Start(&m)
-	audio.WaveOut(&m, "out.raw", 48000)
+	audio.Start(&m)
+	//audio.WaveOut(&m, "out.raw", 48000)
 }
